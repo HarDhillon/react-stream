8242b785350436dde728bf44070810a2558cb229
Har Dhillon <har7173@gmail.com>
2021-03-16 18:33:54 +0400

    adding redux-form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
+import { reducer } from './redux-form';
 import authReducer from './authReucer'
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: reducer
 })
