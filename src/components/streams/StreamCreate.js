efd8dfa238b961132a3051298dd1bfa78e71497d
Har Dhillon <har7173@gmail.com>
2021-03-16 19:11:10 +0400

    hooking up our two form inputs to our redux store

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>
+// this class has a TON of props which are being passed in by reduxForm
+class StreamCreate extends React.Component {
+
+  renderInput(formProps) {
+    // <input
+    //   onChange={formProps.input.onChange}
+    //   value={formProps.input.value}
+    // />
+    // we can shorten the above into below
+    return (
+      <input {...formProps.input } />
+     )
+  }
+
+  render() { 
+    return (
+      <form>
+        {/* the component should be an input, and it gets called with some props through Field */}
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    )
+  }
 }
 
-export default StreamCreate;
+// reduxForm works similarly to Connect, it only receives one object
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
