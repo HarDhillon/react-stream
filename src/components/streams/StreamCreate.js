cffa76e95dae812207fe48239af015cc7eb0da85
Har Dhillon <har7173@gmail.com>
2021-03-17 17:55:36 +0400

    adding redux thunk in, wiring up connect, creating axios base file, creating action that uses redux thunk to post to the json api

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 // this class has a TON of props which are being passed in by reduxForm
 class StreamCreate extends React.Component {
+
   renderError(meta) {
     if (meta.touched && meta.error) {
       return (
@@ -24,18 +27,20 @@ class StreamCreate extends React.Component {
       <div className={className}>
         <label>{formProps.label}</label>
         <input {...formProps.input } autoComplete="off" />
+        {/* meta contains functions like touched */}
         {this.renderError(formProps.meta)}
       </div>
      )
   }
 
-  onSubmit(formValues) {
-    console.log(formValues)
+  // on submit gets called with
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues)
   }
 
   render() { 
     return (
-      // it has error class name for smeantic ui to show errors
+      // it has error class name for semantic ui to show errors
       <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
         {/* the component should be an input, and it gets called with some props through Field */}
         <Field name="title" component={this.renderInput} label="Enter Title" />
@@ -44,6 +49,7 @@ class StreamCreate extends React.Component {
       </form>
     )
   }
+
 }
 
 const validate = (formValues) => {
@@ -61,7 +67,10 @@ const validate = (formValues) => {
 };
 
 // reduxForm works similarly to Connect, it only receives one object
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate: validate
 })(StreamCreate);
+
+// this is how use both connect and reduxForm
+export default connect(null, { createStream: createStream })(formWrapped)
